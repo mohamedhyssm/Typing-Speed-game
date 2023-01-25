3a9779c1f1e0cd4d67106fff60b1d5723c672d05
mohamed haithem <mohamedhyssm2019@gmail.com>
2023-01-25 19:30:13 +0200

    fix best score problems

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,32 @@
 // catch Selectors
 
-let container = document.querySelector(".game .container")
+let container = document.querySelector(".game .container"),
 
-let lvlNameMessage = document.querySelector(".message .lvl");
+    lvlNameMessage = document.querySelector(".message .lvl"),
 
-let bestScore = document.querySelector(".game .best-score .best")
+    bestScore = document.querySelector(".game .best-score .best"),
 
-let secondsSpan = document.querySelector(".message .seconds");
+    secondsSpan = document.querySelector(".message .seconds"),
 
-let startButton = document.querySelector(".start");
+    startButton = document.querySelector(".start"),
 
-let theWord = document.querySelector(".the-word");
+    theWord = document.querySelector(".the-word"),
 
-let upcomingWords = document.querySelector(".upcoming-words");
+    upcomingWords = document.querySelector(".upcoming-words"),
 
-let input = document.querySelector(".input");
+    input = document.querySelector(".input"),
 
-let timeLeftSpan = document.querySelector(".time span");
+    timeLeftSpan = document.querySelector(".time span"),
 
-let scoreGot = document.querySelector(".score .got");
+    scoreGot = document.querySelector(".score .got"),
 
-let scoreTotal = document.querySelector(".score .total");
+    scoreTotal = document.querySelector(".score .total"),
 
-let finishMessage = document.querySelector(".finish");
+    finishMessage = document.querySelector(".finish"),
 
-let checkbox = document.querySelector(".game .container .checkbox")
+    checkbox = document.querySelector(".game .container .checkbox"),
 
-let labelInputRadio = Array.from(document.querySelectorAll(".game .container .checkbox label"))
+    labelInputRadio = Array.from(document.querySelectorAll(".game .container .checkbox label"))
 
 
 // Array Of Words
@@ -203,12 +203,12 @@ function startPlay() {
 function heighScore() {
     if (localStorage.getItem("score")) {
         if (+JSON.parse(localStorage.getItem("score"))[levelName] < +scoreGot.textContent) {
-            score[levelName] = +scoreGot.textContent
+            score[levelName] = `${+scoreGot.textContent}`
             localStorage.setItem("score", JSON.stringify(score))
             bestScore.innerHTML = score[levelName]
         }
     } else {
-        score[levelName] = +scoreGot.textContent
+        score[levelName] = `${+scoreGot.textContent}`
         localStorage.setItem("score", JSON.stringify(score))
     }
 }
@@ -216,76 +216,8 @@ function heighScore() {
 function tryAgain() {
     words.push(...wordsRemoved)
     wordsRemoved = []
-    // div checkbox
-    let div = document.createElement("div")
-    div.className = "checkbox"
-    // span message
-    let span = document.createElement("span")
-    span.classList.add("choose")
-    span.appendChild(document.createTextNode("choose the level : "))
-    // input 
-    let inputProperties = document.createElement("input")
-    inputProperties.type = "radio";
-    inputProperties.name = "lvl";
-    inputProperties.className = "radio-check";
-    // input 1 + label + span
-    let firstInput = inputProperties.cloneNode()
-    firstInput.id = "easy";
-    let firstLabel = document.createElement("label")
-    firstLabel.setAttribute("for", "easy")
-    firstLabel.className = "Easy"
-    firstLabel.appendChild(document.createTextNode("Eazy "))
-    let firstSpan = document.createElement("span")
-    firstSpan.appendChild(document.createTextNode("-"))
-    // input 2 + label + span
-    let secondInput = inputProperties.cloneNode()
-    secondInput.id = "normal";
-    let secondLabel = document.createElement("label")
-    secondLabel.setAttribute("for", "normal")
-    secondLabel.className = "Normal"
-    secondLabel.appendChild(document.createTextNode(" Normal "))
-    let secondSpan = document.createElement("span")
-    secondSpan.appendChild(document.createTextNode("-"))
-    // input 3 + label
-    let thirdInput = inputProperties.cloneNode()
-    thirdInput.id = "hard";
-    let thirdLabel = document.createElement("label")
-    thirdLabel.setAttribute("for", "hard")
-    thirdLabel.className = "Hard"
-    thirdLabel.appendChild(document.createTextNode(" Hard"))
-    // append to div
-    div.append(span)
-    div.append(firstInput)
-    div.append(firstLabel)
-    div.append(firstSpan)
-    div.append(secondInput)
-    div.append(secondLabel)
-    div.append(secondSpan)
-    div.append(thirdInput)
-    div.append(thirdLabel)
     // append to countainer
-    container.append(div)
-
-    // add event click again
-    let labelInputRadio = Array.from(document.querySelectorAll(".game .container .checkbox label"))
-    // choose the level
-    labelInputRadio.forEach(label => {
-        label.onclick = () => {
-            // catch value in class label
-            levelName = label.classList[0];
-            // level name
-            lvlNameMessage.innerHTML = levelName;
-            // level Seconds
-            levelSeconds = lvls[levelName];
-            // span seconds in message
-            secondsSpan.innerHTML = levelSeconds;
-            // span seconds in time Left
-            timeLeftSpan.innerHTML = levelSeconds;
-            if (localStorage.getItem("score")) {
-                bestScore.innerHTML = JSON.parse(localStorage.getItem("score"))[levelName]
-            }
-        }
-    })
+    container.append(checkbox)
     // button try again
     let button = document.createElement("button")
     button.className = "start"
@@ -295,7 +227,7 @@ function tryAgain() {
     button.onclick = function () {
         // remove button checkbox and focus input to start writing
         this.remove();
-        div.remove();
+        checkbox.remove();
         input.focus();
         input.value = ""
         // rest finish message 
